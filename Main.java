a4f842cdb9578c6a94ac3b888eba856cbdc5e519
Yashthorat1912 <yashthorat1912@gmail.com>
2025-07-01 22:05:38 +0530

    different way to dsiplay o/p

---
+++ Main.java
@@ -0,0 +1,14 @@
+public class Main {
+  public static void main(String[] args) {
+    // usingprintln
+    System.out.println("Hello USing println");
+    System.out.println("Hello USing println");
+    // using print
+    System.out.print("Hello USing print");
+    System.out.print("Hello USing print");
+    System.out.println("Hello");
+    // using \n
+    System.out.print("Hello USing backslah n \n");
+    System.out.print("Hello  \n");
+  }
+}
