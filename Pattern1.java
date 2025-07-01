db123471a06e6f61eacbe286f2ef000b921bff10
Yashthorat1912 <yashthorat1912@gmail.com>
2025-07-01 22:09:06 +0530

    Pattern 1

---
+++ Pattern1.java
@@ -0,0 +1,16 @@
+public class Pattern1 {
+  public static void main(String[] args) {
+    /*
+     * pattern
+     *
+     * *
+     * * *
+     * * * *
+     */
+    System.out.println("*");
+    System.out.println("**");
+    System.out.println("***");
+    System.out.println("****");
+  }
+
+}
