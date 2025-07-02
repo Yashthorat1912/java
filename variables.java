8d3bdf9e54bb44918a36efaad7ddf93995ba44d3
Yashthorat1912 <yashthorat1912@gmail.com>
2025-07-02 11:20:54 +0530

    variables

---
+++ variables.java
@@ -0,0 +1,15 @@
+public class variables {
+  public static void main(String[] args) {
+    // variables
+    int a = 10;
+    String name = "Yash";
+    int b = 5;
+    int sum = a + b;
+    System.out.println(sum);
+    System.out.println(name);
+    int ans = (a * b) / (a - b);
+    System.out.println(ans);
+
+  }
+
+}
