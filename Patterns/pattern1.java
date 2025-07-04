895d901af23ec736d5684d872d801f1412789376
Yashthorat1912 <yashthorat1912@gmail.com>
2025-07-04 11:44:48 +0530

    Patterns

---
+++ Patterns/pattern1.java
@@ -0,0 +1,23 @@
+package Patterns;
+
+import java.util.*;
+
+public class pattern1 {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    System.out.print("Enter value of n:");
+    int n = sc.nextInt();
+    System.out.print("Enter value of m:");
+
+    int m = sc.nextInt();
+
+    for (int i = 1; i <= n; i++) {
+      for (int j = 1; j <= m; j++) {
+        System.out.print(" * ");
+
+      }
+      System.out.println();
+    }
+  }
+
+}
