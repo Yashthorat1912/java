f38e59df86bd5234436a22f61d0306301ff04848
Yashthorat1912 <yashthorat1912@gmail.com>
2025-08-06 16:11:10 +0530

    sorting

---
+++ Sorting/selectionSort.java
@@ -0,0 +1,31 @@
+package Sorting;
+
+public class selectionSort {
+  public static void printArray(int[] arr) {
+    for (int i = 0; i < arr.length; i++) {
+      System.out.print(arr[i] + " ");
+    }
+    System.out.println();
+  }
+
+  public static void main(String[] args) {
+    int[] arr = { 7, 8, 3, 1, 2 };
+
+    // selectionSort
+    for (int i = 0; i < arr.length - 1; i++) { // n-1 passes
+      int smallest = i;
+      for (int j = i + 1; j < arr.length; j++) {
+        if (arr[j] < arr[smallest]) {
+          smallest = j; // find the index of the minimum element
+        }
+      }
+      // swap the found minimum element with the first element
+      int temp = arr[smallest];
+      arr[smallest] = arr[i];
+      arr[i] = temp;
+    }
+
+    printArray(arr);
+  }
+
+}
